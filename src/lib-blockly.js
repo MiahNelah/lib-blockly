c210085430c4f6d4bdd018922800c5fca204e272
Daimakaicho <cbouyer@protonmail.com>
2022-05-29 12:05:28 +0200

    add foundry block: send message

--- src/lib-blockly.js
+++ src/lib-blockly.js
@@ -1,6 +1,7 @@
 import { LibBlocky } from "./LibBlockly.js";
 import defaultToolbox from "./blocks/libblocky-toolbox.js";
 import "./blocks/blocks_foundry_roll.js";
+import "./blocks/blocks_foundry_chat.js";
 
 Hooks.once("init", () => {
 
