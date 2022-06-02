5bf73f2fb50b5c98e2e0e9af19d431aadb25b9e4
Miah Nelah <cbouyer@proton.me>
2022-06-02 20:34:15 +0200

    add combat blocks (#19)
    Co-authored-by: Miah Nelah <cbouyer@protonmail.com>

---
+++ src/blocks/blocks_foundry_combat.js
@@ -0,0 +1,376 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "foundry_combat_create_combat",
+        "message0": "Create combat in scene %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "scene",
+                "check": "Scene"
+            }
+        ],
+        "output": "Combat",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_start",
+        "message0": "Start combat %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_activate_combat",
+        "message0": "Activate combat %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_get_active_combat",
+        "message0": "Get active combat",
+        "output": "Combat",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_stop",
+        "message0": "Stop combat %1 %2 Ask confirmation ?",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            },
+            {
+                "type": "field_checkbox",
+                "name": "confirmation",
+                "checked": true
+            }
+        ],
+        "inputsInline": false,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_get_round",
+        "message0": "Get round %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "output": "Number",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_get_turns",
+        "message0": "Get turns %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "output": "Array",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_get_current_turn",
+        "message0": "Get current turn %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "output": "Turn",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_is_active",
+        "message0": "Is combat active ? %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "output": "Boolean",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_reset",
+        "message0": "Reset combat %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_previous_turn",
+        "message0": "Previous turn %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_previous_round",
+        "message0": "Previous round %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_next_turn",
+        "message0": "Next turn %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "foundry_combat_next_round",
+        "message0": "Next round %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "combat",
+                "check": "Combat"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    }
+]);
+
+Blockly.JavaScript['foundry_combat_create_combat'] = function (block) {
+    const scene_name = Blockly.JavaScript.valueToCode(block, 'scene', Blockly.JavaScript.ORDER_ATOMIC);
+    let createCombatHelper = Blockly.JavaScript.provideFunction_("blockly_create_combat_helper", [
+        `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(scene) {`,
+        `  const cls = getDocumentClass("Combat");`,
+        `  const combat = await cls.create({scene: scene?.id})`,
+        `  await combat.activate({render: false});`,
+        `  return combat;`,
+        `}`]);
+    return [`await ${createCombatHelper}(${scene_name})`, Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['foundry_combat_start'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.startCombat();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_stop'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    const checkbox_confirmation = block.getFieldValue('confirmation') === 'TRUE';
+    return `if (${value_name}) await ${value_name}.${checkbox_confirmation ? "endCombat" : "delete"}();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_get_round'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return [`${value_name}?.round`, Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['foundry_combat_get_turns'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return [`${value_name}?.turns`, Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['foundry_combat_is_active'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return [`${value_name}?.isActive`, Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['foundry_combat_reset'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.resetAll();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_delete'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.delete();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_activate_combat'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.activate();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_get_active_combat'] = function (block) {
+    return [`game.combats.active`, Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['foundry_combat_get_current_turn'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return [`${value_name}?.current`, Blockly.JavaScript.ORDER_NONE];
+}
+
+Blockly.JavaScript['foundry_combat_previous_turn'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.previousTurn();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_previous_round'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.previousRound();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_next_turn'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.nextTurn();\n`;
+}
+
+Blockly.JavaScript['foundry_combat_next_round'] = function (block) {
+    const value_name = Blockly.JavaScript.valueToCode(block, 'combat', Blockly.JavaScript.ORDER_ATOMIC);
+    return `if (${value_name}) await ${value_name}.nextRound();\n`;
+}
+
+
+const toolbox = [
+    {
+        "kind": "category",
+        "name": "Combat",
+        "contents": [
+            {
+                "kind": "block",
+                "type": "foundry_combat_create_combat"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_start"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_activate_combat"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_get_active_combat"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_stop"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_get_round"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_get_turns"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_get_current_turn"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_is_active"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_reset"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_previous_turn"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_previous_round"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_next_turn"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_combat_next_round"
+            }
+        ]
+    }
+]
+
+Hooks.once('ready', () => {
+    // TODO: find a better way to add custom blocks than this...
+    game.modules.get("libblockly").instance.toolbox()
+        .contents // root contents
+        .find(x => x.name === "Foundry").contents // foundry category contents
+        .push(...toolbox);
+})
+
--- src/blocks/blocks_foundry_token.js
+++ src/blocks/blocks_foundry_token.js
@@ -11,66 +11,82 @@ Blockly.defineBlocksWithJsonArray([
         "type": "foundry_token_change_visibility",
         "message0": "Toggle token's visibility %1",
         "args0": [
-          {
-            "type": "input_value",
-            "name": "token"
-          }
+            {
+                "type": "input_value",
+                "name": "token"
+            }
         ],
         "previousStatement": null,
         "nextStatement": null,
         "colour": 230,
         "tooltip": "",
         "helpUrl": ""
-      },
-      {
+    },
+    {
         "type": "foundry_token_hide",
         "message0": "Hide tokens %1",
         "args0": [
-          {
-            "type": "input_value",
-            "name": "token"
-          }
+            {
+                "type": "input_value",
+                "name": "token"
+            }
         ],
         "previousStatement": null,
         "nextStatement": null,
         "colour": 230,
         "tooltip": "",
         "helpUrl": ""
-      },
-      {
+    },
+    {
         "type": "foundry_token_show",
         "message0": "Show tokens %1",
         "args0": [
-          {
-            "type": "input_value",
-            "name": "token"
-          }
+            {
+                "type": "input_value",
+                "name": "token"
+            }
         ],
         "previousStatement": null,
         "nextStatement": null,
         "colour": 230,
         "tooltip": "",
         "helpUrl": ""
-      },
-      {
+    },
+    {
         "type": "foundry_token_select_tokens",
         "message0": "Select tokens in scene %1",
         "args0": [
-          {
-            "type": "input_value",
-            "name": "tokens",
-            "check": [
-              "Array",
-              "Token"
-            ]
-          }
+            {
+                "type": "input_value",
+                "name": "tokens",
+                "check": [
+                    "Array",
+                    "Token"
+                ]
+            }
         ],
         "previousStatement": null,
         "nextStatement": null,
         "colour": 230,
         "tooltip": "",
         "helpUrl": ""
-      }
+    },
+    {
+        "type": "foundry_token_toggle_combat_state",
+        "message0": "Toggle combat state %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "token",
+                "check": ["Array", "Token"]
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    }
 ]);
 
 Blockly.JavaScript["foundry_token_get_selected_tokens"] = function (block) {
@@ -79,15 +95,15 @@ Blockly.JavaScript["foundry_token_get_selected_tokens"] = function (block) {
 
 Blockly.JavaScript["foundry_token_change_visibility"] = function (block) {
     var token_input = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC);
-    
+
     const tokenVisibilityeHelper = Blockly.JavaScript.provideFunction_("blockly_token_toggle_visibility", [
-      `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
-      `  if(Array.isArray(token) && token.length > 0) {`,
-      `    token.forEach(async t => await t.toggleVisibility());`,
-      `  } else if (token instanceof Token) {`,
-      `    await token.toggleVisibility();`,
-      `  }`,
-      `}`
+        `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
+        `  if(Array.isArray(token) && token.length > 0) {`,
+        `    token.forEach(async t => await t.toggleVisibility());`,
+        `  } else if (token instanceof Token) {`,
+        `    await token.toggleVisibility();`,
+        `  }`,
+        `}`
     ]);
 
     return `await ${tokenVisibilityeHelper}(${token_input});\n`;
@@ -97,40 +113,55 @@ Blockly.JavaScript["foundry_token_hide"] = function (block) {
     var token_input = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC);
 
     const tokenHideeHelper = Blockly.JavaScript.provideFunction_("blockly_token_hide", [
-      `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
-      `  if(Array.isArray(token) && token.length > 0) {`,
-      `    await canvas.scene.updateEmbeddedDocuments("Token", token.map(t => { return {_id: t.id, hidden: true}}));`,
-      `  } else if (token instanceof Token) {`,
-      `    await canvas.scene.updateEmbeddedDocuments("Token", {_id: token.id, hidden: true});`,
-      `  }`,
-      `}`
+        `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
+        `  if(Array.isArray(token) && token.length > 0) {`,
+        `    await canvas.scene.updateEmbeddedDocuments("Token", token.map(t => { return {_id: t.id, hidden: true}}));`,
+        `  } else if (token instanceof Token) {`,
+        `    await canvas.scene.updateEmbeddedDocuments("Token", {_id: token.id, hidden: true});`,
+        `  }`,
+        `}`
     ]);
     return `await ${tokenHideeHelper}(${token_input});\n`;
 }
 
 Blockly.JavaScript["foundry_token_show"] = function (block) {
     var token_input = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC);
-    
+
     const tokenShoweHelper = Blockly.JavaScript.provideFunction_("blockly_token_show", [
-      `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
-      `  if(Array.isArray(token) && token.length > 0) {`,
-      `    await canvas.scene.updateEmbeddedDocuments("Token", token.map(t => { return {_id: t.id, hidden: false}}));`,
-      `  } else if (token instanceof Token) {`,
-      `    await canvas.scene.updateEmbeddedDocuments("Token", {_id: token.id, hidden: false});`,
-      `  }`,
-      `}`
+        `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
+        `  if(Array.isArray(token) && token.length > 0) {`,
+        `    await canvas.scene.updateEmbeddedDocuments("Token", token.map(t => { return {_id: t.id, hidden: false}}));`,
+        `  } else if (token instanceof Token) {`,
+        `    await canvas.scene.updateEmbeddedDocuments("Token", {_id: token.id, hidden: false});`,
+        `  }`,
+        `}`
     ]);
 
     return `await ${tokenShoweHelper}(${token_input});\n`;
 }
 
 Blockly.JavaScript["foundry_token_select_tokens"] = function (block) {
-  var tokens_value =  Blockly.JavaScript.valueToCode(block, 'tokens', Blockly.JavaScript.ORDER_ATOMIC);
-  var tempVar = Blockly.JavaScript.variableDB_.getDistinctName('temp', Blockly.Variables.NAME_TYPE);
+    var tokens_value = Blockly.JavaScript.valueToCode(block, 'tokens', Blockly.JavaScript.ORDER_ATOMIC);
+    var tempVar = Blockly.JavaScript.variableDB_.getDistinctName('temp', Blockly.Variables.NAME_TYPE);
 
-  let code = `let ${tempVar} = ${tokens_value};\n`;
-  code += `if (${tempVar}) canvas.tokens.selectObjects(${tempVar});\n`;
-  return code;
+    let code = `let ${tempVar} = ${tokens_value};\n`;
+    code += `if (${tempVar}) canvas.tokens.selectObjects(${tempVar});\n`;
+    return code;
+}
+
+Blockly.JavaScript["foundry_token_toggle_combat_state"] = function (block) {
+    var token_input = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC);
+
+    const toggleCombatStateHelper = Blockly.JavaScript.provideFunction_("blockly_token_toggle_combat_state_helper", [
+        `async function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(token) {`,
+        `  if(Array.isArray(token) && token.length > 0) {`,
+        `    token.forEach(async t => await t.toggleCombat());`,
+        `  } else if (token instanceof Token) {`,
+        `    await token.toggleCombat();`,
+        `  }`,
+        `}`
+    ]);
+    return `if (${token_input}) await ${toggleCombatStateHelper}(${token_input});\n`;
 }
 
 const toolbox = [
@@ -157,6 +188,10 @@ const toolbox = [
             {
                 "kind": "block",
                 "type": "foundry_token_select_tokens"
+            },
+            {
+                "kind": "block",
+                "type": "foundry_token_toggle_combat_state"
             }
         ]
     }
