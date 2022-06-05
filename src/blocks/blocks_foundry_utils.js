1ca0736381c238714ff4a8853eed7bf92ef74646
Miah Nelah <cbouyer@protonmail.com>
2022-06-05 10:33:12 +0200

    rewrite and localize Utils category

--- src/blocks/blocks_foundry_utils.js
+++ src/blocks/blocks_foundry_utils.js
@@ -1,49 +1,63 @@
-Blockly.defineBlocksWithJsonArray([
-    {
-        "type": "foundry_utils_delay",
-        "message0": "Wait %1 %2",
-        "args0": [
-            {
-                "type": "field_number",
-                "name": "delay",
-                "value": 1,
-                "min": 0
-            },
-            {
-                "type": "field_dropdown",
-                "name": "units",
-                "options": [
-                    ["s", "s"],
-                    ["ms", "ms"]
-                ]
-            }
-        ],
-        "previousStatement": null,
-        "nextStatement": null,
-        "colour": 230,
-        "tooltip": "",
-        "helpUrl": ""
+class WaitCustomBlock {
+    constructor() {
+        this.kind = "block";
+        this.key = "foundry_utils_delay";
+        this.category = "Foundry.Utils";
     }
-]);
 
-Blockly.JavaScript["foundry_utils_delay"] = function (block) {
-    const number_delay = block.getFieldValue('delay');
-    const dropdown_units = block.getFieldValue('units');
+    /**
+     *
+     * @return {!Object}
+     */
+    init() {
+        return {
+            "message0": game.i18n.localize("LibBlockly.Blocks.Utils.Wait.Title"),
+            "args0": [
+                {
+                    "type": "field_number",
+                    "name": "delay",
+                    "value": 1,
+                    "min": 0
+                },
+                {
+                    "type": "field_dropdown",
+                    "name": "units",
+                    "options": [
+                        [game.i18n.localize("LibBlockly.Blocks.Utils.Wait.Seconds"), "s"],
+                        [game.i18n.localize("LibBlockly.Blocks.Utils.Wait.Milliseconds"), "ms"]
+                    ]
+                }
+            ],
+            "previousStatement": null,
+            "nextStatement": null,
+            "colour": game.i18n.localize("LibBlockly.Blocks.Utils.Wait.Colour"),
+            "tooltip": game.i18n.localize("LibBlockly.Blocks.Utils.Wait.Tooltip"),
+            "helpUrl": game.i18n.localize("LibBlockly.Blocks.Utils.Wait.HelpUrl"),
+        }
+    }
 
-    const delayHelper = Blockly.JavaScript.provideFunction_("blockly_delay_helper", [
-        `function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(delay) {`,
-        `  return new Promise(resolve => {`,
-        `    setTimeout(() => resolve(2), delay);`,
-        `  });`,
-        `}`
-    ]);
+    /**
+     *
+     * @param {!Blockly.BlockSvg} block
+     */
+    generateCode(block) {
+        const number_delay = block.getFieldValue('delay');
+        const dropdown_units = block.getFieldValue('units');
 
-    return `await ${delayHelper}(${dropdown_units === "s" ? number_delay*1000 : number_delay});\n`;
+        const delayHelper = Blockly.JavaScript.provideFunction_("blockly_delay_helper", [
+            `function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(delay) {`,
+            `  return new Promise(resolve => {`,
+            `    setTimeout(() => resolve(2), delay);`,
+            `  });`,
+            `}`
+        ]);
+
+        return `await ${delayHelper}(${dropdown_units === "s" ? number_delay * 1000 : number_delay});\n`;
+    }
 }
 
 Hooks.once('ready', () => {
-    game.modules.get("libblockly")
-        .toolboxManager.getCategory("Foundry", true)
-        .addCategory("Utils")
-        .addBlock("block", "foundry_utils_delay");
+    game.modules.get("libblockly").blockManager.register([
+        new WaitCustomBlock()
+    ]);
 })
