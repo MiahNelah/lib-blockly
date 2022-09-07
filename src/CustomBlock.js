a800adf5d0a92197452de9cc1dd235de0ffe0867
Miah Nelah <cbouyer@protonmail.com>
2022-09-07 22:32:06 +0200

    add more customisation options to CustomBlock

--- src/CustomBlock.js
+++ src/CustomBlock.js
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 export class CustomBlock {
     /**
      * 
@@ -6,22 +9,42 @@ export class CustomBlock {
      * @param {!Object} definition 
      * @param {String} kind 
      */
-    constructor(name, category, kind = "block") {
-        this.kind = kind;
-        this.category = category;
-        this.key = `${this.category}.${name}`;
-        this.definition = libBlockly.getDefinition(this.key);
-        if (!this.definition) throw Error(`Custom block defnition not found: ${this.key}`);
+    constructor(name, category, options = { kind: "block", localisationPrefix: "LibBlockly.Blocks" }) {
+        this._kind = options.kind;
+        this._localisationPrefix = options.localisationPrefix;
+        this._category = category;
+        this._key = `${this._category}.${name}`;
+        this._definition = libBlockly.getDefinition(this._key);
+        if (!this._definition) throw Error(`Custom block defnition not found: ${this._key}`);
     }
 
+    /**
+     * 
+     */
+    get kind() { return this._kind; }
+
+    /**
+     * 
+     */
+    get category() { return this._category; }
+
+    /**
+     * 
+     */
+    get key() { return this._key; }
+
+    /**
+     * 
+     * @returns 
+     */
     init() {
         return mergeObject({
-            "colour": game.i18n.localize(`LibBlockly.Blocks.${this.key}.Colour`),
-            "tooltip": game.i18n.localize(`LibBlockly.Blocks.${this.key}.Tooltip`),
-            "helpUrl": game.i18n.localize(`LibBlockly.Blocks.${this.key}.HelpUrl`),
-            "message0": game.i18n.localize(`LibBlockly.Blocks.${this.key}.Title`),
+            "colour": game.i18n.localize(`${this._localisationPrefix}.${this._key}.Colour`),
+            "tooltip": game.i18n.localize(`${this._localisationPrefix}.${this._key}.Tooltip`),
+            "helpUrl": game.i18n.localize(`${this._localisationPrefix}.${this._key}.HelpUrl`),
+            "message0": game.i18n.localize(`${this._localisationPrefix}.${this._key}.Title`),
             "inputsInline": false,
-        }, this.definition);
+        }, this._definition);
     }
 
     /**
